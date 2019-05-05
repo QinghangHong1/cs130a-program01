0fa35965d80d694904c26cae69954d2d694445ee
Qinghang Hong <qhong@umail.ucsb.edu>
2019-05-04 17:55:37 -0700

    finish first time

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,1 +1,64 @@
+#include <cstdlib>
+#include <iostream>
 #include "BloomFilter.h"
+
+using namespace std;
+BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
+    this -> k = k;
+    this -> m = m;
+    bits = new uint64_t;
+    *bits = *bits & 0;
+    if(strfn == "JenkinsHash"){
+        this -> strfn = new JenkinsHash();
+    }
+    if(strfn == "PearsonHash"){
+        this -> strfn = new PearsonHash();
+    }else{
+        cerr << "Not a valid stringHash" << endl;
+        exit(1);
+    }
+    this -> intfns = new IntegerHash*[k];
+    if(intfn == "DivisionHash"){
+        for(int i = 0; i < k; i++){
+        this -> intfns[i] = new DivisionHash(i, m);
+        }
+    }else if(intfn == "ReciprocalHash"){
+        for(int i = 0; i < k; i++){
+            this -> intfns[i] = new ReciprocalHash(i, m);
+        }
+    }else if(intfn == "SquareRootHash"){
+        for(int i = 0; i < k; i++){
+            this -> intfns[i] = new SquareRootHash(i, m);
+        }
+    }else{
+        cerr << "Invalid StringHash function" << endl;
+        exit(1);
+    }
+        
+}
+BloomFilter::~BloomFilter(){
+    delete strfn;
+    for(int i = 0; i < k; i++){
+        delete [](intfns[i]);
+    }
+    delete []intfns;
+    delete bits;
+}
+void BloomFilter::insert(const string& value){
+    uint64_t stringHashValue = strfn ->  hash(value);
+    for(int i = 0; i < k; i++){
+        *bits = *bits | (1 << intfns[i]-> hash(stringHashValue));
+    }
+
+}
+bool BloomFilter::lookup(const string& value) const{
+    uint64_t stringHashValue = strfn -> hash(value);
+    for(int i = 0; i < k; i++){
+        uint64_t hashIndex = intfns[i] -> hash(stringHashValue);
+        uint64_t bit = 2 ^ hashIndex;
+        if((*bits & bit) == 0){
+            return false;
+        }
+    }
+    return true;
+}
--- HashSet.cpp
+++ HashSet.cpp
@@ -1,1 +1,90 @@
 #include "HashSet.h"
+using namespace std;
+HashSet::HashSet(){
+    nitems = 0;
+    nslots = 97;
+    intfn = new SquareRootHash(1, nslots);
+    strfn = new JenkinsHash();
+    slots = new string* [nslots];
+    for(int i = 0; i < nslots; i++){
+        slots[i] = NULL;
+    }
+}
+HashSet::~HashSet(){
+    delete intfn;
+    delete strfn;
+    delete strfn2;
+    for(int i = 0; i < nslots; i++){
+        if(slots[i] != NULL){
+            delete slots[i];
+        }
+    }
+    delete []slots;
+}
+void HashSet::insert(const string& value){
+    if(nslots <= (nitems * 2)){
+        rehash();
+    }
+    int stringToIntHash = strfn -> hash(value);
+    uint64_t hashValue = intfn -> hash(stringToIntHash) % nslots;
+    if(slots[hashValue] == NULL){
+        slots[hashValue] = new string(value);
+    }else{
+        int n = 1;
+        int newHashValue = hashValue;
+        while(slots[hashValue] != NULL){
+            newHashValue = (hashValue + n * n) % nslots;
+            n++; 
+        }
+        slots[newHashValue] = new string(value);
+    }
+    nitems ++;
+
+}
+bool HashSet::lookup(const string& value) const{
+    int stringToIntHash = strfn -> hash(value);
+    int hashValue = intfn -> hash(stringToIntHash) % nslots;
+    if(slots[hashValue] == NULL){
+        return false;
+    }else{
+        int newHashValue = hashValue + 1;
+        int n = 2;
+        while(slots[newHashValue] != NULL){
+            if(*slots[newHashValue] != value){
+                newHashValue = hashValue + n * n;
+                n++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
+void HashSet::rehash(){
+    string** newTable = new string* [nslots * 2 + 1];
+    int newNslots = nslots * 2 + 1;
+    for(int i = 0; i < newNslots; i++){
+        newTable[i] = NULL;
+    }
+    for(int i = 0; i < nslots; i++){
+        if(slots[i] != NULL){
+            string value = *slots[i];
+            int stringToIntHash = strfn -> hash(value);
+            int hashValue = intfn -> hash(stringToIntHash) % newNslots;
+            if(newTable[hashValue] == NULL){
+                newTable[hashValue] = new string(value);
+            }else{
+                int n = 1;
+                int newHashValue = hashValue;
+                while(newTable[hashValue] != NULL){
+                    newHashValue = (hashValue + n * n) % newNslots ; 
+                    n ++;
+                }
+                newTable[newHashValue] = new string(value);
+            }
+        }
+    }
+    slots = newTable;
+    nslots = newNslots;
+
+}
