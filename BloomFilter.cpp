e7f458614975c8dfee8428907294d9844aa237f3
Qinghang Hong <qhong@umail.ucsb.edu>
2019-05-05 21:52:53 -0700

    change bloomFilter

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -7,8 +7,8 @@ using namespace std;
 BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
     this -> k = k;
     this -> m = m;
-    bits = new uint64_t[m];
-    for(int i = 0; i < m; i++){
+    bits = new uint64_t[m / 64 + 1];
+    for(int i = 0; i < (m / 64 +1); i++){
         bits[i] = 0;
     }
     if(strfn == "jenkins"){
@@ -41,7 +41,7 @@ BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
 BloomFilter::~BloomFilter(){
     delete strfn;
     for(int i = 0; i < k; i++){
-        delete intfns[i];
+        delete intfns[i]; 
     }
     delete []intfns;
     delete []bits;
@@ -50,40 +50,20 @@ void BloomFilter::insert(const string& value){
     uint64_t stringHashValue = strfn ->  hash(value);
     for(int i = 0; i < k; i++){
         uint64_t intHash = intfns[i]-> hash(stringHashValue);
-        uint64_t index = intHash % (m);
-        bits[index] = 1;
-        // int index = (intHash / 64) % m;
-        // int position = index % 64;
-        // cout << "beforechange " << bitset<64>(bits[index]) << endl;
-        // cout << "setting " << position << "th bit to 1" << endl;
-        // bits[index] |= (1 << position);
-        // cout << "After change " <<bitset<64>(bits[index]) << endl;
-       
+        uint64_t index = (intHash / 64) ;
+        int position = index % 64;
+        bits[index] |= (1 << position);
     }
-
 }
 bool BloomFilter::lookup(const string& value) const{
     uint64_t stringHashValue = strfn -> hash(value);
     for(int i = 0; i < k; i++){
         uint64_t intHash = intfns[i]-> hash(stringHashValue);
-        // int index = (intHash / 64) % m;
-        // int position = index % 64;
-        // if(!(bits[index] & (1 << position))){
-        //     return false;
-        // }
-        uint64_t index = intHash % m; 
-        if (bits[index] == 0){
+        uint64_t index = (intHash / 64) ;
+        uint64_t digit = index % 64;
+        if((bits[index] &= (1 << (digit))) != 1){
             return false;
         }
-        // uint64_t index = intHash % (m * 64);
-        // uint64_t digit = index / 64;
-        // int numBit = index - digit *  64;
-        // cout << "the bit are " << bitset<64>(bits[digit]) << endl;
-        // uint64_t a = bits[digit] & (1 << numBit);
-        // cout << "Compare bits are " << bitset<64>(a) << endl;
-        // if(!a){
-        //     return false;
-        // }
     }
     return true;
 }
